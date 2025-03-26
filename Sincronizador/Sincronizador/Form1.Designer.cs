7e8c37c3b054bb71126e700380b23104d1c3973b
Omar Enrique Aguilar Mojica <ia.aguilaromar5@ufg.edu.sv>
2025-03-26 11:24:15 -0600

    Version 3

--- Sincronizador/Sincronizador/Form1.Designer.cs
+++ Sincronizador/Sincronizador/Form1.Designer.cs
@@ -29,13 +29,16 @@ namespace Sincronizador
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnSincronizar = new System.Windows.Forms.Button();
             this.progressBarSync = new System.Windows.Forms.ProgressBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnSincronizar
             // 
-            this.btnSincronizar.Location = new System.Drawing.Point(227, 202);
+            this.btnSincronizar.Location = new System.Drawing.Point(227, 276);
             this.btnSincronizar.Name = "btnSincronizar";
             this.btnSincronizar.Size = new System.Drawing.Size(330, 146);
             this.btnSincronizar.TabIndex = 0;
@@ -45,22 +48,36 @@ namespace Sincronizador
             // 
             // progressBarSync
             // 
-            this.progressBarSync.Location = new System.Drawing.Point(12, 116);
+            this.progressBarSync.Location = new System.Drawing.Point(12, 238);
             this.progressBarSync.Name = "progressBarSync";
             this.progressBarSync.Size = new System.Drawing.Size(776, 23);
             this.progressBarSync.TabIndex = 1;
             this.progressBarSync.Click += new System.EventHandler(this.progressBarSync_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Sincronizador.Properties.Resources._7541812;
+            this.pictureBox1.Location = new System.Drawing.Point(301, 33);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 189);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.progressBarSync);
             this.Controls.Add(this.btnSincronizar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sincronizador";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,5 +86,6 @@ namespace Sincronizador
 
         private System.Windows.Forms.Button btnSincronizar;
         private System.Windows.Forms.ProgressBar progressBarSync;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
